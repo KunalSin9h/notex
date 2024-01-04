9cf8b90f08459eb91d0bbe63fc5138e19714b11b
Kunal Singh <kunalsin9h@gmail.com>
2024-01-04 23:11:35 +0530

    feat: added all 9 api routes
    Signed-off-by: Kunal Singh <kunalsin9h@gmail.com>

---
+++ rateLimiting.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"time"
+
+	"github.com/gofiber/fiber/v2/middleware/limiter"
+)
+
+// These are defaults, the values can be changes using env vars
+// to make it simple i have hard corded it
+var (
+	D_MAX        = 15               // nolint
+	D_EXPIRATION = 30 * time.Second // nolint
+)
+
+func rateLimiterConfig() *limiter.Config {
+	// Rate limiter can be ignored for local development
+	/*
+		To ignore rate limiter use this property in the config
+
+		Next: func(c *fiber.Ctx) bool {
+			return c.IP() == "127.0.0.1"
+		},
+	*/
+
+	config := limiter.Config{
+		Max:        D_MAX, // {Max} request in per {Expiration} interval
+		Expiration: D_EXPIRATION,
+	}
+
+	return &config
+}
--- routes.go
+++ routes.go
@@ -4,6 +4,10 @@ import (
 	"fmt"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/encryptcookie"
+	"github.com/gofiber/fiber/v2/middleware/limiter"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/swagger"
 )
 
@@ -18,8 +22,58 @@ func (app *Config) run() {
 func (app *Config) routes() *fiber.App {
 	routes := fiber.New()
 
+	// CORS settings
+	routes.Use(cors.New())
+
+	// Logger for logging every API Request
+	routes.Use(logger.New())
+
 	// Swagger 2.0 API Documentation
 	routes.Get("/swagger/*", swagger.HandlerDefault)
+
+	// Request Throttling (Rate limiting API Access)
+	// Default is 15 request per 30 Second window by single IP address
+	routes.Use(limiter.New(*rateLimiterConfig()))
+
+	// Encrypted Cookies for secure Authentication from the Browser
+	routes.Use(encryptcookie.New(encryptcookie.Config{
+		Key: encryptcookie.GenerateKey(),
+		// A new Key is generated every-time which will make current cookies invalid
+		// to prevent from this, use a persisted key (from env vars maybe)
+	}))
+
+	routes.Get("/", func(c *fiber.Ctx) error {
+		return c.JSON(routes.Stack())
+	})
+
+	//------------------
+	// APIs
+	//-----------------
+	api := routes.Group("/api")
+
+	//------------------
+	// Auth APIs
+	//------------------
+	auth := api.Group("/auth")
+	auth.Post("/signup")
+	auth.Post("/login")
+
+	//------------------
+	// Notes APIs
+	//------------------
+	notes := api.Group("/notes")
+	notes.Get("/")
+	notes.Get("/:id")
+	notes.Post("/")
+	notes.Put("/:id")
+	notes.Delete("/:id")
+	notes.Post("/:id/share")
+
+	//------------------
+	// Search APIs
+	//------------------
+	search := api.Group("/search")
+	search.Get("/")
 
 	return routes
 }
